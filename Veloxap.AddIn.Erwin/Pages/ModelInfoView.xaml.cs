6d99f1abf71074622f16f887f69f53f25b776c6b
aykut <aykut.baytop@mdsaptech.com>
2026-07-02 09:21:21 +0300

    * model bilgileri kaldırıldı

--- Veloxap.AddIn.Erwin/Pages/ModelInfoView.xaml.cs
+++ Veloxap.AddIn.Erwin/Pages/ModelInfoView.xaml.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Threading;
 using Veloxap.AddIn.Erwin.Models;
 using Veloxap.AddIn.Erwin.Services;
 using Veloxap.AddIn.Erwin.ViewModels;
@@ -10,14 +8,12 @@ namespace Veloxap.AddIn.Erwin.Pages
 {
     public partial class ModelInfoView : UserControl
     {
-        private const int SearchDelayMilliseconds = 500;
         private readonly ModelInfo tableUdpModelInfo;
         private readonly SCAPI.Application tableUdpApplication;
         private readonly SCAPI.PersistenceUnit tableUdpPersistenceUnit;
         private readonly RuleService catalogRuleService;
         private readonly string catalogName;
         private readonly string catalogLongId;
-        private readonly DispatcherTimer searchTimer;
         private bool hasLoadedCatalogOverview;
 
         public ModelInfoView()
@@ -55,12 +51,6 @@ namespace Veloxap.AddIn.Erwin.Pages
             this.catalogName = catalogName;
             this.catalogLongId = catalogLongId;
 
-            searchTimer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromMilliseconds(SearchDelayMilliseconds)
-            };
-            searchTimer.Tick += SearchTimer_Tick;
-
             InitializeComponent();
             Loaded += ModelInfoView_Loaded;
             tabModelSections.SelectionChanged += TabModelSections_SelectionChanged;
@@ -69,11 +59,8 @@ namespace Veloxap.AddIn.Erwin.Pages
                 ? new ModelInfoViewModel()
                 : new ModelInfoViewModel(modelInfo);
 
-            if (showTableUdpTab)
-            {
-                tabModelSections.SelectedItem = tabTableUdps;
-                EnsureTableUdpViewLoaded();
-            }
+            tabModelSections.SelectedItem = tabTableUdps;
+            EnsureTableUdpViewLoaded();
         }
 
         private async void ModelInfoView_Loaded(object sender, RoutedEventArgs e)
@@ -118,15 +105,6 @@ namespace Veloxap.AddIn.Erwin.Pages
                 catalogLongId);
         }
 
-        private void ModelTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
-        {
-            var viewModel = DataContext as ModelInfoViewModel;
-            if (viewModel == null)
-                return;
-
-            viewModel.SelectedModelItem = e.NewValue as ModelInfoViewModel.ModelDisplayItem;
-        }
-
         private async void BtnUnlockCatalog_Click(object sender, RoutedEventArgs e)
         {
             var viewModel = DataContext as ModelInfoViewModel;
@@ -137,63 +115,6 @@ namespace Veloxap.AddIn.Erwin.Pages
                 catalogRuleService,
                 catalogName,
                 catalogLongId);
-        }
-
-        private void TxtSearch_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (searchTimer == null)
-            {
-                ApplyFilter();
-                return;
-            }
-
-            searchTimer.Stop();
-            searchTimer.Start();
-        }
-
-        private void SearchTimer_Tick(object sender, EventArgs e)
-        {
-            searchTimer.Stop();
-            ApplyFilter();
-        }
-
-        private void ModelTreeViewItem_Loaded(object sender, RoutedEventArgs e)
-        {
-            var item = sender as TreeViewItem;
-            if (item == null)
-                return;
-
-            var node = item.DataContext as ModelInfoViewModel.ModelDisplayItem;
-            if (node == null || node.IsPlaceholder || !node.IsExpanded)
-                return;
-
-            node.EnsureChildrenLoaded();
-        }
-
-        private void ModelTreeViewItem_Expanded(object sender, RoutedEventArgs e)
-        {
-            var item = e.OriginalSource as TreeViewItem;
-            if (item == null)
-                return;
-
-            var node = item.DataContext as ModelInfoViewModel.ModelDisplayItem;
-            if (node == null || node.IsPlaceholder)
-                return;
-
-            node.EnsureChildrenLoaded();
-        }
-
-        private void ApplyFilter()
-        {
-            var viewModel = DataContext as ModelInfoViewModel;
-            if (viewModel == null)
-                return;
-
-            string filter = txtSearch == null
-                ? string.Empty
-                : txtSearch.Text;
-
-            viewModel.ApplyFilter(filter);
         }
     }
 }
